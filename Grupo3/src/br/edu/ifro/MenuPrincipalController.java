003d80872ed739593b55b2b50f037bc76ee3fbd8
04574926279 <04574926279@JIPA-LABIN03-08.ifro.local>
2018-05-04 16:57:44 -0400

    Fazendo calculadora

--- Grupo3/src/br/edu/ifro/MenuPrincipalController.java
+++ Grupo3/src/br/edu/ifro/MenuPrincipalController.java
@@ -7,21 +7,32 @@ package br.edu.ifro;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
-/**
- * FXML Controller class
- *
- * @author 04574926279
- */
 public class MenuPrincipalController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+   @FXML
+   private TextField txtnumer1, txtnumero2,txtresultado;
+
+   @FXML
+   private Button btnsoma;
+   
+   @FXML
+   private void soma(ActionEvent evens){
+       Double num1 = Double.parseDouble(txtnumer1.getText());
+       Double num2 = Double.parseDouble(txtnumero2.getText());
+       Double resultado = Double.parseDouble(txtresultado.getText());
+       resultado =num1 +num2;
+       
+       txtresultado.setText(resultado.toString());
+   }
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+
     }    
     
 }
